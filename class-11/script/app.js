c2da6a6620a4359f83176f33edb389be55419117
Sawaira <125546250+Sawaira-dev@users.noreply.github.com>
2023-04-06 22:10:35 +0500

    Intro to JS and git terminal

---
+++ class-11/script/app.js
@@ -0,0 +1,52 @@
+// var username =prompt("Your name is" ,"Usama")
+// console.log(username)
+
+// var variable se start nh hoga, darmiyan me spaces nh de skty
+// java case sensitive hai
+
+// var Sawaira is ko variable banana hai
+// code browser k console pr likhna ha q k phir error dhondna muskil ho jai ga
+
+// var exp =8+7 (7/9) 
+// DMAS rule k mutabik breakits use krni ha warna result glt aiga 
+// console.log(exp)
+
+// var age =+ prompt("Your age")
+// var Fage =+ prompt("Your father's age")
+// alert(age+Fage)
+
+// DAY 1 - 6 April 
+
+// alert('Hello World')
+// var username =prompt("Your Name PLease","Usama")
+// var user = "Usama"
+// var fuser = "Usman"
+// console.log("Hello " + user+" " +fuser)
+
+// var sawera rasheed
+// saweraRasheed camelCase
+// SaweraRasheed PascalCase
+// sawera_rasheed snake_case
+
+// console.log(2+5)
+// console.log(2-5)
+// console.log(2*5)
+// console.log(2/5)
+// console.log(2**5)
+
+// var exp = (8 + (7 / 9) - (10 / 7) * (4 - 6)) + 4
+// console.log(exp)
+
+// var age =+ prompt("Your age PLease")
+// var fage =+ prompt("Your father age PLease")
+// alert(age+fage)
+// alert(Number(age) + Number(fage))
+// console.log(typeof age)
+
+
+var zakat_percent= 8.025
+var user_asset = prompt ("Asset Value")
+var result= zakat_percent*user_asset
+alert("Your zakat="+result )
+
+// Day 2 - 7 Apr
